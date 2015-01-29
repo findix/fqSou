dcf6eac34ea7e6d5e7682b78c5d90698d762d0c4
Sean Fung <fengxiang220@gmail.com>
2015-01-29 19:05:27 +0800

    First Commit
    Signed-off-by: Sean Fung <fengxiang220@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# fqSou
+# fqSou —— 一个很烂的shadowsocks管理系统
+[http://fqsou.com](http://fqsou.com)
+
+## 部署方式
+Python Tornado项目
+使用Supervisor部署
+
+1. 安装pip
+1. 安装tornado，supervisor
+1. 安装supervisor
+1. 创建Shadowsocks配置文件shadowsocks.json //`_comment`是注释给自己看的
+		{
+		    "server": "fqsou.com",
+		    "port_password": {
+		        "8388": "xxxxxxx",
+		    },
+		    "_comment": {
+		        "8388": "myself",
+		    },
+		    "local_address": "127.0.0.1",
+		    "local_port": 1080,
+		    "timeout": 300,
+		    "method": "aes-256-cfb",
+		    "fast_open": false
+		}
+1. 修改supervisor配置文件
+最后加入以下内容
+
+		[program:fqSou]
+		command=python fqSou.py
+		directory=/opt/fqSou
+		autostart=true
+		autorestart=true
+		exitcodes=0
+		stopsignal=KILL
+		redirect_stderr=true
+		numprocs=1
+		process_name=%(program_name)s_%(process_num)02d
+
+输入通过8000端口即可访问，后面Nginx做个反向代理就随意了
+管理员账号密码在config.properties文件中，下面是shadowsocks.json文件的路径，请按实际情况设置。
+
+感谢[Shadowsocks](https://github.com/shadowsocks/shadowsocks "Shadowsocks")项目！
---
+++ fqSou.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import os
+import subprocess
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+import Config
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+    def get(self):
+        self.write_error(404)
+
+    def write_error(self, status_code, **kwargs):
+        if status_code == 404:
+            self.render('404.html')
+        else:
+            self.write('error:' + str(status_code))
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.render("login.html")
+
+    def post(self):
+        username = self.get_argument("username")
+        password = self.get_argument("password")
+        if username == Config.read_config("config.properties", "admin",
+                                          "admin_username") and password == Config.read_config("config.properties",
+                                                                                               "admin", "admin_passwd"):
+            self.set_secure_cookie("user", username)
+        self.redirect("/admin")
+
+
+class LogoutHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.set_secure_cookie("user", "")
+        self.redirect("/")
+
+
+class ChangePasswdHandler(BaseHandler):
+    @tornado.web.authenticated
+    def post(self):
+        password = self.get_argument("password")
+        Config.write_config("config.properties", "admin", "admin_passwd", password)
+        self.redirect("/admin")
+
+
+class AdminHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        print Config.read_config("config.properties", "config", "shadowsocks_config")
+        json = open(Config.read_config("config.properties", "config", "shadowsocks_config"), "r")
+        self.render("admin.html", json=json.read())
+
+
+class SaveHandler(BaseHandler):
+    @tornado.web.authenticated
+    def post(self):
+        json = self.get_argument("json")
+        config = open(Config.read_config("config.properties", "config", "shadowsocks_config"), "w")
+        config.write(json)
+        config.close()
+        status = subprocess.call("ssserver -c /etc/shadowsocks.json -d restart", shell=True)
+        self.write("成功" if status == 0 else "失败")
+
+
+class LogHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        log = open(r"/var/log/port-ip-monitor.log", "r").read()
+        self.render("log.html", log=log.replace("\n", "<br/>"))
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    application = tornado.web.Application(
+        handlers=[
+            (r"/", MainHandler),
+            (r"/login", LoginHandler),
+            (r"/logout", LogoutHandler),
+            (r"/changepasswd", ChangePasswdHandler),
+            (r"/admin", AdminHandler),
+            (r"/save", SaveHandler),
+            (r"/log", LogHandler),
+            (r".*", BaseHandler),
+        ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        debug=False,
+        login_url=r"/login",
+        cookie_secret="61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo="
+    )
+    application.listen(3000)
+    tornado.ioloop.IOLoop.instance().start()
